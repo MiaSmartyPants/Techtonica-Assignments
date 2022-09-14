02b84fbd422e1f65b164d5d6a2cfa85318735988
Mia Dixon <mia.dixon143@gmail.com>
2022-09-14 10:30:21 -0500

    problems

--- quiz-app/client/src/App.js
+++ quiz-app/client/src/App.js
@@ -1,28 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
+import {useEffect} from 'react' 
 
 
 function App() {
   const [question, setQuestion] = useState("");
-
-// how does the fron end know that /miatrivia is the endpoint that i wanted
+  const[error, setError]= useState("");
+// DEFINE here
 function getData() {
-  fetch("/miatrivia")
+  fetch("/localhost:PORT/miatrivia")
+  
   //return the response
-    .then((response) => response)
+    .then((response) => console.log(response.json()))
     // with the data that was return store it n a variable
+    
     .then((data) => {
       console.log(data, 'something');
       setQuestion(data);
     });
-    console.log(question);
-}
+   }
+    // .catch (error => {
+    //   console.error("Error fetching data: ", error);
+    //   setError(error);
+    // })
+    //console.log(question);
+//}
+
+// how does the fron end know that /miatrivia is the endpoint that i wanted
+//a: because you tell it - technically it should be `"http://localhost:PORT/miatrivia" - its the same thing as `https://www.linkedin.com/feed` or `https://facebook.com/device`
+useEffect(() => {
+ // CALL here
+  getData()
+},[])
+//getData();
   return (
     <div className="App">
     <h1>Trivia Game</h1>
-<p>{question}</p>
-
     </div>
   );
 }
--- quiz-app/server/server.js
+++ quiz-app/server/server.js
@@ -14,18 +14,38 @@ app.use(cors());
 app.use(express.json());
 
 let trivia ;
-request('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean', 
+request('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean',
  (error, response, body) => {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  trivia = body;
-});
-
-app.get('/miatrivia', (req, res) => {
+  //trivia = body;
+  const trivia = JSON.parse(body);
+ console.log('trivia:', trivia); 
   
+});
+ 
+app.get('/miatrivia', (req, res) => {
+ 
   res.send(trivia)
 })
+
+// app.get('/miatrivia', (req, res) => {
+//   // request needs to be called each time
+//   request('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean', 
+//  (error, response, body) => {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+
+//   // convert body to JSON - there are many different types of 
+//   const json = JSON.parse(body);
+//   console.log('json:', json); // Print the HTML for the Google homepage.
+
+//   //I'm not sure what the `send` does - but this is how you return json
+//   res.json(json)
+// });
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
