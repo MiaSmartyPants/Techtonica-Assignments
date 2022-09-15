237e6b0d9833c4222ea73b7183f862b451cbb43d
Mia Dixon <mia.dixon143@gmail.com>
2022-09-15 16:25:47 -0500

    fully workingneed to ad css

--- quiz-app/client/src/App.js
+++ quiz-app/client/src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import Confetti from 'react-confetti';
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
 import {useEffect} from 'react' 
 
+import Popup from './components/popup.js';
+import Timer from  './components/Timer.js';
+
+//put inside function to generate/display questionNumlater
+//make questionNum a state so it renders for the next page
+
 
 function App() {
-  const [question, setQuestion] = useState("");
+  const [questionNum, setQuestion] = useState(Math.floor(Math.random() * 10));
+  const [results, setResults] = useState("");
+  const [counter, setCounter] = useState(0);
   const[error, setError]= useState("");
 // DEFINE here
 async function getData() {
@@ -13,38 +23,98 @@ async function getData() {
   
   //return the response
     .then((response) => response)
-    // with the data that was return store it n a variable
-    
+    // with the data that was return store it questionNuma variable
     .then((data) => {
-     
-     
       return data.json();
-      // setQuestion(data);
     })
+    .then((data)=> {
+      
+      // dont use questionNum until calling it in p tag
+      setQuestion(Math.floor(Math.random() * 10))
+    setResults(data.results[questionNum])
+    let display = data.results[questionNum];
+    console.log("questionNum ", display)
 
-    .then((data)=>console.log(data));
-
-    
+})
    }
 
-    // .catch (error => {
-    //   console.error("Error fetching data: ", error);
-    //   setError(error);
-    // })
-    //console.log(question);
-//}
-
+//if value of button === questionNu.correct_answer
 // how does the fron end know that /miatrivia is the endpoint that i wanted
 //a: because you tell it - technically it should be `"http://localhost:PORT/miatrivia" - its the same thing as `https://www.linkedin.com/feed` or `https://facebook.com/device`
 useEffect(() => {
  // CALL here
-  getData()
+  getData() 
+  
+  
 },[])
-//getData();
+
+
+function correctAnswer(userAnswer){
+  if( userAnswer==results.correct_answer){//
+    setCounter(counter+1)
+    getData();
+    console.log(questionNum)
+
+    //funfetti
+  
+  }else{
+    setCounter(counter-1)
+     getData();
+    console.log(questionNum)
+  //something awful here
+  }
+ }
+
+
+
+
+
+
+
+
+
+    const [isOpen, setIsOpen] = useState(false);
+ 
+    const togglePopup = () => {
+      setIsOpen(!isOpen);
+    }
+
+//set highscore to counter
+//somehow keep from refreshing when page is reset
+//function tokeep track of high score
+//store counter into high score
+//if current score>highscore{
+  //sethighscore=current score
+//disply"new high score!"}
+//else display highscore
+
+
+
+
   return (
     <div className="App">
-    <h1>Trivia Game</h1>
+      <h1>Trivia Game </h1>
+       <p>{results.question}</p>       
+       <button onClick={()=>correctAnswer("True")}>True</button>
+       <button onClick={()=>correctAnswer("False")}>False</button>
+       <h2>Points: {counter}</h2>
+        <Timer setIsOpen={setIsOpen}/>
+      
+    {isOpen && <Popup
+      content={<>
+        <b>Current Score {counter}</b>
+        <p> High Score {counter} </p>
+        <a href="/">
+        <button{...setIsOpen(False)}>Play Again</button>
+        </a>
+      </>}
+      handleClose={togglePopup}
+    />}
+
+
+
     </div>
+
   );
 }
 
